4fd16e293a9883b17acb090d344b58e06229f708
AlirieGray <alirie.gray@gmail.com>
2022-11-18 23:21:16 -0800

    set up mongodb and user routes

---
+++ src/api/resources/user/routes.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import controller from './controller'
+
+const router = express.Router()
+
+router.post('/create', controller.createUser)
+router.get('/get/:userID', controller.readUser)
+router.get('/get/', controller.readAll)
+router.patch('/get/:userID', controller.updateUser)
+router.delete('/get/:userID', controller.deleteUser)
+
+export = router
